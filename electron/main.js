2403130131bfa6446b532c55c9f2fa3a0fb69177
K122611 <K122611@users.noreply.github.com>
2026-06-10 18:41:17 +0800

    fix: 修正Electron生产模式加载路径 dist/renderer

--- electron/main.js
+++ electron/main.js
@@ -47,7 +47,7 @@ function createMainWindow() {
     mainWindow.loadURL('http://localhost:5173');
     mainWindow.webContents.openDevTools();
   } else {
-    mainWindow.loadFile(path.join(__dirname, '../dist/index.html'));
+    mainWindow.loadFile(path.join(__dirname, '../dist/renderer/index.html'));
   }
 
   const { session } = require('electron');
